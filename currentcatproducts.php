e366fecad1b27963dd0444a0c4163365890cdc27
pavluk <info@art-pavluk.com>
2018-05-04 15:03:52 +0300

    Initial commit

---
+++ currentcatproducts.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package    Current category products Plugin
+ * @version    1.0.0
+ * @author     Artem Pavluk - www.art-pavluk.com
+ * @copyright  Copyright (c) 2010 - 2018 Private master Pavluk. All rights reserved.
+ * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
+ * @link       https://art-pavluk.com
+ */
+
+defined('_JEXEC') or die;
+
